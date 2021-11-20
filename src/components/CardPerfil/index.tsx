7ad340e30862df36ea1f42c9d3f53e2d4b495f2b
leonardo.assuncao <leonardo.assuncao>
2021-11-20 10:01:29 -0300

    Fix

--- src/components/CardPerfil/index.tsx
+++ src/components/CardPerfil/index.tsx
@@ -1,29 +1,35 @@
 import { Avatar, Button, Card, Col, Divider, Input, Row } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 
+interface ICardPerfil {
+  nome: string;
+  local: string;
+  profissao: string;
+  experiencia: string;
+}
 
-export const CardPerfil = ({nome, local, profissao, experiencia}) => {
-  return(
-    <Card 
-    hoverable
-    title={
-      <Row gutter={25}>
-        <Col span={8}>
-          <Avatar size={50} icon={<UserOutlined />} />
-        </Col>
-        <Col span={16}>
-          <Row>
-            <Col>
-              <span style={{ fontSize: '10pt', fontWeight: 'bold' }}>{nome}</span></Col>
-          </Row>
-          <Row>
-            <Col>
-              <span style={{ fontSize: '8pt', }}>{local}</span>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-    }>
+export const CardPerfil = ({ nome, local, profissao, experiencia }: ICardPerfil) => {
+  return (
+    <Card
+      hoverable
+      title={
+        <Row gutter={25}>
+          <Col span={8}>
+            <Avatar size={50} icon={<UserOutlined />} />
+          </Col>
+          <Col span={16}>
+            <Row>
+              <Col>
+                <span style={{ fontSize: '10pt', fontWeight: 'bold' }}>{nome}</span></Col>
+            </Row>
+            <Row>
+              <Col>
+                <span style={{ fontSize: '8pt', }}>{local}</span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      }>
       <Row justify="space-between">
         <Col>
           <p>{profissao}</p>
