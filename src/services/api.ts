a4c2b5b7565e2460f6e4d40252c070c5592de19c
leonardo.assuncao <leonardo.assuncao>
2021-11-23 11:51:35 -0300

    Add api ibge para buscar localidades

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export const Api = axios.create({
   baseURL: "https://bico-app-api.herokuapp.com/"
+  // baseURL: "http://localhost:3001/"
+})
+
+export const ApiLocate = axios.create({
+  baseURL: "https://servicodados.ibge.gov.br/api/v1/localidades/"
 })
