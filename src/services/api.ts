01ae6be1c631dd3c88e065ec871fdccc5b5baec4
leonardo.assuncao <leonardo.assuncao>
2021-11-25 23:23:25 -0300

    Add interceptor on request (token)

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getUserLocalStorage } from "../context/AuthProvider/util";
 
 export const Api = axios.create({
   baseURL: "https://bico-app-api.herokuapp.com/"
@@ -8,3 +9,18 @@ export const Api = axios.create({
 export const ApiLocate = axios.create({
   baseURL: "https://servicodados.ibge.gov.br/api/v1/localidades/"
 })
+
+Api.interceptors.request.use(
+  (config) => {
+    const user = getUserLocalStorage()
+
+    const token = `Barer ${user?.token}`
+
+    config.headers.Authorization = token
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
